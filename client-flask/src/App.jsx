84e0008f07c365c3887c02b1ee84db8dd76f758c
Eloi-Cortiella <eloicortiella@iesebre.com>
2026-01-15 17:38:19 +0100

    Revert "Crida a API"
    This reverts commit 1b7ad1d86a25142f2b4c83c43f31aeae40b26f37.

---
+++ client-flask/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch('http://localhost:5000/api/users');
+      const data = await res.json();
+      setUsers(data.users);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <h2>Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
