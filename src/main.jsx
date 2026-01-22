68222b1ca773aaf156743cef572786af4b776ba0
miqueldieguez <miqueldieguez@iesebre.com>
2026-01-22 20:25:42 +0100

    Arreglar el login

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LoginSingup from './components/LoginSingup/LoginSingup.jsx'
+import LoginSingup from './screens/login/LoginSingup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
