fa58ba6bc9a9b147dd9919d20e8249832bc241e1
Eloi-Cortiella <eloicortiella@iesebre.com>
2026-01-29 18:52:47 +0100

    Afegit component de SideBar per a la gestio del compte

---
+++ components/SideBar.jsx
@@ -0,0 +1,15 @@
+function SideBar() {
+    return (
+        <div>
+            <nav> {/* Navegació per diferents seccions del perfil. Separar-lo a la zona  */}
+                <span>
+                    <h3>Informacio Personal</h3>
+                    <h3>Configuracio del compte</h3>
+                    <h3>Pagaments i subscripcions</h3>
+                </span>
+            </nav>
+        </div>
+    )
+}
+
+export default SideBar;
--- src/screens/perfil/Perfil.jsx
+++ src/screens/perfil/Perfil.jsx
@@ -1,27 +1,20 @@
 import './Perfil.css'
-import profileLogo from '../../assets/prova.jpg'
+import SideBar from '../../../components/SideBar'
+import profileLogo from '../../assets/aura.png'
 
 function Perfil() {
   return (
     <>
       <div>
-        <h1>Perfil</h1>
+        <h1>Gestionar compte</h1>
         <img src={profileLogo} className="w-md rounded-xl" alt="Foto de perfil" />
 
         <div>
           <h2>Nom d'usuari</h2> {/* Aquí s'hauria d'inserir el nom d'usuari dinàmicament */}
           <p>Correu electrònic</p> {/* Aquí s'hauria d'inserir el correu electrònic dinàmicament */}
           <button>Editar perfil</button> {/* Botó per editar el perfil */}
-          <div>
-            <nav> {/* Navegació per diferents seccions del perfil. Separar-lo a la zona  */}
-              <span>
-                <h3>Informacio Personal</h3>
-                <h3>Configuracio del compte</h3>
-                <h3>Pagaments i subscripcions</h3>
-              </span>
-            </nav>
-          </div>
         </div>
+        <SideBar />
       </div>
     </>
   )
